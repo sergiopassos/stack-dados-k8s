71ff974fb7b82b2afac5865527cc43c1351ce2f9
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2024-02-23 15:30:17 -0300

    reduce

--- data/dags/sql_transform.py
+++ data/dags/sql_transform.py
@@ -70,30 +70,8 @@ def dbt_sql_transform():
             }
         )
 
-        tg_stg_postgres = DbtTaskGroup(
-            group_id="tg_stg_postgres",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.AUTOMATIC, select=[f"tag:postgres"]),
-            profile_config=profile_config,
-            operator_args={
-                "install_deps": True,
-                "vars": {}
-            }
-        )
 
-        tg_stg_mongodb = DbtTaskGroup(
-            group_id="tg_stg_mongodb",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.AUTOMATIC, select=[f"tag:mongodb"]),
-            execution_config=ExecutionConfig(dbt_project_path=dbt_root_path),
-            profile_config=profile_config,
-            operator_args={
-                "install_deps": True,
-                "vars": {}
-            }
-        )
-
-        [tg_stg_mssql, tg_stg_postgres, tg_stg_mongodb]
+        [tg_stg_mssql]
 
 
     get_metadata >> stage

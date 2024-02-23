e2ac27fc1d7db6f28c5ce4022e89b444e8a08a2e
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2024-02-23 09:15:26 -0300

    add custom airflow image

--- app/data-gen-datastores/object_storage.bash
+++ app/data-gen-datastores/object_storage.bash
@@ -1,6 +1,6 @@
-cd ""
+cd "/Users/luanmorenomaciel/GitHub/ws-stack-dados-k8s/app/data-gen-datastores/"
 
-for i in {1..100}
+for i in {1..1000}
 do
-   python cli.py all
+   python cli.py all parquet
 done

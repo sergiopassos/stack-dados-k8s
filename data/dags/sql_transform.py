c58b41d8be5b7671f26123b763a6ac5dce79d7f5
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2024-02-23 14:16:29 -0300

    mode

--- data/dags/sql_transform.py
+++ data/dags/sql_transform.py
@@ -52,46 +52,42 @@ profile_config = ProfileConfig(
 def dbt_sql_transform():
     """
     """
-
-    with TaskGroup(group_id="stage") as stage:
         
-        tg_stg_mssql = DbtTaskGroup(
-            group_id="tg_stg_mssql",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:mssql"]),
-            profile_config=profile_config,
-            operator_args={
-                "install_deps": True,
-                "vars": {}
-            }
-        )
+    tg_stg_mssql = DbtTaskGroup(
+        group_id="tg_stg_mssql",
+        project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
+        render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:mssql"]),
+        profile_config=profile_config,
+        operator_args={
+            "install_deps": True,
+            "vars": {}
+        }
+    )
 
-        tg_stg_postgres = DbtTaskGroup(
-            group_id="tg_stg_postgres",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:postgres"]),
-            profile_config=profile_config,
-            operator_args={
-                "install_deps": True,
-                "vars": {}
-            }
-        )
+    tg_stg_postgres = DbtTaskGroup(
+        group_id="tg_stg_postgres",
+        project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
+        render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:postgres"]),
+        profile_config=profile_config,
+        operator_args={
+            "install_deps": True,
+            "vars": {}
+        }
+    )
 
-        tg_stg_mongodb = DbtTaskGroup(
-            group_id="tg_stg_mongodb",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:mongodb"]),
-            profile_config=profile_config,
-            operator_args={
-                "install_deps": True,
-                "vars": {}
-            }
-        )
+    tg_stg_mongodb = DbtTaskGroup(
+        group_id="tg_stg_mongodb",
+        project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
+        render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:mongodb"]),
+        profile_config=profile_config,
+        operator_args={
+            "install_deps": True,
+            "vars": {}
+        }
+    )
 
-        tg_stg_mssql >> tg_stg_postgres >> tg_stg_mongodb
-
-
-    stage
+    
+    tg_stg_mssql >> tg_stg_postgres >> tg_stg_mongodb
 
 
 dbt_sql_transform()

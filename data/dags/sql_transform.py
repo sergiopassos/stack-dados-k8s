08dcc5cdc9601a3c5c9361e8d60b26452df11c2c
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2024-02-23 15:37:24 -0300

    remove automatic

--- data/dags/sql_transform.py
+++ data/dags/sql_transform.py
@@ -61,7 +61,7 @@ def dbt_sql_transform():
         tg_stg_mssql = DbtTaskGroup(
             group_id="tg_stg_mssql",
             project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.AUTOMATIC, select=[f"tag:mssql"]),
+            render_config=RenderConfig(select=[f"tag:mssql"]),
             execution_config=ExecutionConfig(dbt_project_path=dbt_root_path),
             profile_config=profile_config,
             operator_args={

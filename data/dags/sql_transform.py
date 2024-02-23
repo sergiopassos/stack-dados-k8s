c4852ee2bf57c69e7e569738f769c61ee44cca10
Luan Moreno Medeiros Maciel <luan.moreno@owshq.com>
2024-02-23 14:51:40 -0300

    addd manifest

--- data/dags/sql_transform.py
+++ data/dags/sql_transform.py
@@ -31,6 +31,7 @@ default_args = {
 
 default_dbt_root_path = Path(__file__).parent / "dbt"
 dbt_root_path = Path(os.getenv("DBT_ROOT_PATH", default_dbt_root_path))
+dbt_manifest = Path(dbt_root_path, "manifest.json")
 
 profile_config = ProfileConfig(
     profile_name="iceberg",
@@ -57,8 +58,8 @@ def dbt_sql_transform():
         
         tg_stg_mssql = DbtTaskGroup(
             group_id="tg_stg_mssql",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:mssql"]),
+            project_config=ProjectConfig(manifest_path=dbt_manifest, project_name="owshq"),
+            render_config=RenderConfig(load_method=LoadMode.DBT_MANIFEST, select=[f"tag:mssql"]),
             profile_config=profile_config,
             operator_args={
                 "install_deps": True,
@@ -68,8 +69,8 @@ def dbt_sql_transform():
 
         tg_stg_postgres = DbtTaskGroup(
             group_id="tg_stg_postgres",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.DBT_LS, select=[f"tag:postgres"]),
+            project_config=ProjectConfig(manifest_path=dbt_manifest, project_name="owshq"),
+            render_config=RenderConfig(load_method=LoadMode.DBT_MANIFEST, select=[f"tag:postgres"]),
             profile_config=profile_config,
             operator_args={
                 "install_deps": True,
@@ -79,8 +80,8 @@ def dbt_sql_transform():
 
         tg_stg_mongodb = DbtTaskGroup(
             group_id="tg_stg_mongodb",
-            project_config=ProjectConfig((dbt_root_path / "owshq").as_posix()),
-            render_config=RenderConfig(load_method=LoadMode.CUSTOM, select=[f"tag:mongodb"]),
+            project_config=ProjectConfig(manifest_path=dbt_manifest, project_name="owshq"),
+            render_config=RenderConfig(load_method=LoadMode.DBT_MANIFEST, select=[f"tag:mongodb"]),
             profile_config=profile_config,
             operator_args={
                 "install_deps": True,
